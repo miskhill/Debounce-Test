0c671eb8814182445cacd5ae8d13a544bc6ecb0b
miskhill <miskhill@hotmail.com>
2022-04-17 21:39:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,14 @@ const updateDebounceText = debounce((text) => {
     debounceText.textContent = text
 })
 
+const updateThrottleText = throttle((text) => {
+    throttleText.textContent = text
+})
+
 input.addEventListener("input", e => {
     defaultText.textContent = e.target.value
     updateDebounceText(e.target.value)
+    updateThrottleText(e.target.value)
 })
 
 function debounce (cb, delay = 1000) {
@@ -20,5 +25,22 @@ function debounce (cb, delay = 1000) {
         timeout = setTimeout(() => {
             cb(...args)
         }, delay);
+    }
+}
+
+function throttle (cb, delay = 1000) {
+    let shouldWait = false
+    let waitingArgs 
+    return (...args) => {
+        if(shouldWait) {
+            waitingArgs = args
+            return
+        } 
+        cb(...args)
+        shouldWait = true
+
+        setTimeout(() => {
+        shouldWait = false
+        }, delay)
     }
 }
